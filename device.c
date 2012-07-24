427484e2b77523a63bc5a72998bf7adedc7a0715
Mark Hills <mark@pogo.org.uk>
2012-07-24 22:46:19 +0100

    Specify maximum buffer size

--- device.c
+++ device.c
@@ -44,8 +44,8 @@ int set_alsa_hw(snd_pcm_t *pcm,
 	CHK("snd_pcm_hw_params_set_channels", r);
 
 	dir = 0;
-	r = snd_pcm_hw_params_set_buffer_time_near(pcm, hw, &buffer, &dir);
-	CHK("snd_pcm_hw_params_set_buffer_time_near", r);
+	r = snd_pcm_hw_params_set_buffer_time_max(pcm, hw, &buffer, &dir);
+	CHK("snd_pcm_hw_params_set_buffer_time_max", r);
 
 	r = snd_pcm_hw_params(pcm, hw);
 	CHK("hw_params", r);
